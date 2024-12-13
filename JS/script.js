63c9b079bd3eac985fd2145efe0c6ab9b4341d9c
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-12-13 00:14:18 -0300

    Calculadora finalizada

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,18 @@
-let display = document.getElementsByClassName('display')[0];
-function displayShow(value) {
+let display = document.querySelector('div.display');
+
+function displayNumbers(value) {
     if(value === 'C') {
         display.textContent = ''
     } else if(value === '=') {
-        console.log('Ta dando certo');
+        equation();
     }
      else {
         display.textContent += value;
     }
+}
+
+function equation() {
+    let equation = display.textContent;
+    display.textContent = eval(equation);
 }
 
